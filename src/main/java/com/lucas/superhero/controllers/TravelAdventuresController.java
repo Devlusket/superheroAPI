f05e45d392e36cdcd0e078bdd92fd81700cac272
Devlusket <tsuunakage@gmail.com>
2026-01-16 19:59:06 -0300

    fix: origin path

--- src/main/java/com/lucas/superhero/controllers/TravelAdventuresController.java
+++ src/main/java/com/lucas/superhero/controllers/TravelAdventuresController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.lucas.superhero.entities.Adventure;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/traveladventures")  
 public class TravelAdventuresController {
